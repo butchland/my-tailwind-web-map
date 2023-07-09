644c7be28127581ab128931d184952d79eb82903
Butch Landingin <butchland@gmail.com>
2023-07-10 00:52:54 +0800

    chore:Add sidebar to webmap

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,8 +6,11 @@ function Index() {
       <div className="absolute h-10 w-full z-10 bg-white">
         <header className="block">Header</header>
       </div>
-      <div className="relative h-full">
-        <WebMap/>
+      <div className="relative top-0 flex h-full">
+        <div className="w-32 mt-10 bg-slate-200">Side</div>
+        <div className="relative w-full h-full">
+          <WebMap/>
+        </div>
       </div>
     </div> );
 }

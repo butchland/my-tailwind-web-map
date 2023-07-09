eef10af8b34e4be50d325b4803b9f215b5635335
Butch Landingin <butchland@gmail.com>
2023-07-09 20:46:13 +0800

    chore:Get basic map impl working

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,44 @@
+import DeckGL from '@deck.gl/react/typed';
+import Map from 'react-map-gl'
+import '../styles/mapbox-gl.css'
+import { useState } from 'react';
+
+const MAPBOX_TOKEN = import.meta.env.VITE_MAPBOX_API_KEY;
+const mapboxStyle = "mapbox://styles/mapbox/dark-v10";
+const INITIAL_VIEW_STATE = {
+  longitude: 121.141825,
+  latitude: 14.126985,
+  zoom: 14,
+  minZoom: 5,
+  maxZoom: 16,
+  pitch: 45,
+  bearing: 0
+
+}
 function Index() {
+  const handleWebGlInitialize = (gl: WebGLRenderingContext) => {
+    gl.enable(gl.DEPTH_TEST);
+    gl.depthFunc(gl.LEQUAL);
+  };
+  const [viewState, setViewState] = useState(INITIAL_VIEW_STATE);
+  const handleChangeViewState = (args: { viewState: any }) => {
+    const {viewState} = args;
+    //console.log(`zoom: ${JSON.stringify(viewState.zoom)}`)
+    setViewState(viewState);
+  }
+
   return (
-    <>
-      <h1>Index Page</h1>
-    </>
+    <DeckGL
+      initialViewState={viewState}
+      onViewStateChange={handleChangeViewState}
+      controller
+      onWebGLInitialized={handleWebGlInitialize}
+    >
+      <Map
+        mapboxAccessToken={MAPBOX_TOKEN}
+        mapStyle={mapboxStyle} 
+      />
+    </DeckGL>
   );
 }
 

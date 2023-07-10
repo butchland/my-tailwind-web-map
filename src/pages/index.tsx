0003a70fbc941f393620fd5184c724727a9329e2
Butch Landingin <butchland@gmail.com>
2023-07-10 20:39:15 +0800

    try:Add zoom controls

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,16 @@
 import WebMap
  from "../components/WebMap";
+import '../styles/mapbox-gl.css'
+const ButtonBar = () => {
+  return (
+    <div className="flex justify-between h-full">
+    <button className="w-8 mapboxgl-ctrl-zoom-in" type="button" aria-label="Zoom in" aria-disabled="false">
+      <span className="block w-8 h-full mapboxgl-ctrl-icon" aria-hidden="true" title="Zoom in">
+      </span>
+    </button>      
+    </div>
+  );
+}
 function Index() {
   return (
     <div className="relative h-screen">
@@ -10,6 +21,9 @@ function Index() {
         <div className="w-32 mt-10 bg-slate-200">Side</div>
         <div className="relative w-full h-full">
           <WebMap/>
+          <div className="absolute bottom-10 left-4 w-20 h-8 bg-red-400">
+            <ButtonBar/>
+          </div>
         </div>
       </div>
     </div> );

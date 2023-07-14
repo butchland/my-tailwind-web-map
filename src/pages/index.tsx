bc57647c2e5780f4b4b71ea61f88673aa2ca1844
Butch Landingin <butchland@gmail.com>
2023-07-15 01:06:10 +0800

    feat:Use and configure setup sectioned web map component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
+import { NavigationControl } from "react-map-gl";
 import SectionedWebMap from "../components/SectionedWebMap";
+import "../styles/sectioned-webmap.css"
 
 const INITIAL_VIEW_STATE = {
   longitude: 121.141825,
@@ -13,11 +15,28 @@ const mapboxStyle = "mapbox://styles/mapbox/dark-v10";
 
 
 function Index() {
+  const headerContent = <header className="h-full bg-slate-500">Header</header>;
+  const sidebarContent = <div className="bg-slate-500 w-2/12">Sidebar</div>
+  const footerContent = <div className="bg-slate-500">Footer</div>
+  const rightSidebarContent = <div className=" bg-slate-500 w-2/12">Right sidebar content</div>
+  const boxContent = <div className="absolute rounded-lg px-5 text-base py-4 bottom-48 right-10 w-80 h-80 bg-slate-400" >
+                      Map box content
+                     </div>
   return (
     <SectionedWebMap 
       initialViewState={INITIAL_VIEW_STATE}
       mapboxStyle={mapboxStyle}
-    />
+      headerContent={headerContent}
+      sidebarContent={sidebarContent}  
+      footerContent={footerContent}
+      rightSidebarContent={rightSidebarContent}  
+      className="bg-slate-500"
+    >
+      <NavigationControl
+         position="top-left"
+      />
+      {boxContent}
+    </SectionedWebMap>
   );
 }
 
